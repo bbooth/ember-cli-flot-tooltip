c5d8ae5763e10d6a73c129d3f365107942ac4b39
Bradley Booth <bradley.booth@gmail.com>
2015-02-06 16:34:31 -0700

    Added ember-cli-flot as a dependency

--- blueprints/ember-cli-flot-tooltip/index.js
+++ blueprints/ember-cli-flot-tooltip/index.js
@@ -1,7 +1,9 @@
 module.exports = {
-  normalizeEntityName: function() {
+  normalizeEntityName: function () {
   },
-
+  beforeInstall: function () {
+    return this.addAddonToProject('ember-cli-flot', '~0.0.2');
+  },
   afterInstall: function () {
     return this.addBowerPackageToProject('flor.tooltip', '~0.8.4');
   }
